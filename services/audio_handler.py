167232122c0febf20de5ef1d2d7d2e7b36025215
asmmel <asmmel@yandex.ru>
2025-03-13 12:34:20 +0300

    update preffics fot video

--- services/audio_handler.py
+++ services/audio_handler.py
@@ -42,7 +42,7 @@ class AudioHandler:
         self,
         input_path: str,
         output_path: str,
-        min_silence_len: int = 20,
+        min_silence_len: int = 100,
         silence_thresh: int = -40
     ):
         """Удаление пауз из аудио"""
@@ -54,7 +54,7 @@ class AudioHandler:
                 audio,
                 min_silence_len=min_silence_len,
                 silence_thresh=silence_thresh,
-                keep_silence=35
+                keep_silence=100
             )
             
             # Соединение сегментов
